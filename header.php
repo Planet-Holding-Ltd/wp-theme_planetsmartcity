e338f6c1abaf8da2481710aa983f303f2087d7a9
Alessandro Olivero <a.olivero@planetidea.it>
2022-06-09 10:13:39 +0200

    feat: set HTTP security headers

--- header.php
+++ header.php
@@ -12,6 +12,22 @@
  */
 
 ?>
+
+<?php
+
+/**
+ * HTTP security headers.
+ */
+header("X-XSS-Protection: 1; mode=block");
+header("X-Content-Type-Options: nosniff");
+header("Referrer-Policy: same-origin");
+header("Feature-Policy: geolocation 'self'; vibrate 'none'");
+header("Content-Security-Policy: default-src 'unsafe-inline' 'unsafe-eval' https: 'self' data:; upgrade-insecure-requests; frame-ancestors 'self';");
+header("Strict-Transport-Security: max-age=31536000; includeSubDomains");
+header("Permissions-Policy: accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), cross-origin-isolated=(), display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=(), gamepad=(), speaker-selection=(), conversion-measurement=(), focus-without-user-activation=(), hid=(), idle-detection=(), interest-cohort=(), serial=(), sync-script=(), trust-token-redemption=(), window-placement=(), vertical-scroll=()")
+
+?>
+
 <!doctype html>
 <html <?php language_attributes(); ?>>
 
